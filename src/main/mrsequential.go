b1aabd912e2aff8152d46da76dbdaac778b131e1
Jolene <zhiliao0807@163.com>
2024-03-20 20:21:56 +0800

    修改Map与Reduce函：Map返回单词切片，Reduce返回单词频次

--- src/main/mrsequential.go
+++ src/main/mrsequential.go
@@ -67,64 +67,6 @@ func main() {
 	ofile.Close()
 }
 
-/*
-func main1() {
-	//接收命令行参数
-	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: mrsequential xxx.so inputfiles...\n")
-		os.Exit(1)
-	}
-
-	mapf, reducef := loadPlugin(os.Args[1])
-
-	intermediate := []mr.KeyValue{}
-	//遍历txt
-	for _, filename := range os.Args[2:] {
-		file, err := os.Open(filename)
-		if err != nil {
-			log.Fatalf("cannot open %v", filename)
-		}
-		content, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatalf("cannot read %v", filename)
-		}
-		file.Close()
-
-		kva := mapf(filename, string(content))
-		intermediate = append(intermediate, kva...)
-	}
-
-	//字典排序
-	sort.Sort(ByKey(intermediate))
-	oname := "mr-out-0"
-	ofile, _ := os.Create(oname)
-
-	//组装 统计word出现频次
-	i := 0
-	for i < len(intermediate) {
-		j := i + 1
-		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
-			j++
-		}
-		values := []string{}
-		for k := i; k < j; k++ {
-			values = append(values, intermediate[k].Value)
-		}
-		output := reducef(intermediate[i].Key, values)
-
-		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
-
-		i = j
-	}
-
-	ofile.Close()
-}
-*/
-
-//
-// load the application Map and Reduce functions
-// from a plugin file, e.g. ../mrapps/wc.so
-//
 func loadPlugin(filename string) (func(string, string) []string, func(string, int) string) {
 	p, err := plugin.Open(filename)
 	if err != nil {
